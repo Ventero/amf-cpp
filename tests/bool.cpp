413d40fb41e7264aaef8214966f39a8157931b55
Ventero <ventero@ventero.de>
2013-05-18 16:05:29 +0200

    Add simple test for bool

---
+++ tests/bool.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+
+#include "amf.hpp"
+#include "serializer.hpp"
+#include "types/amfbool.hpp"
+
+static void isEqual(const std::vector<u8>& expected, bool value) {
+	ASSERT_EQ(expected, AmfBool(value).serialize());
+}
+
+TEST(BoolSerializationTest, SimpleValues) {
+	isEqual(v8 { 0x02 }, false);
+	isEqual(v8 { 0x03 }, true);
+}
