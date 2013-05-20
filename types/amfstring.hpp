32a6e57917b68e09b4a68edfcf7529d6ceff1b66
Ventero <ventero@ventero.de>
2013-05-20 22:09:05 +0200

    Added AmfString + tests

--- types/amfstring.hpp
+++ types/amfstring.hpp
@@ -4,6 +4,7 @@
 
 #include <string>
 #include "amfitem.hpp"
+#include "amfinteger.hpp"
 
 class AmfString : public AmfItem {
 public:
@@ -11,9 +12,23 @@ public:
 	operator std::string() const { return value; }
 
 	std::vector<u8> serialize() const {
-		return std::vector<u8>{
-			AMF_STRING
-		};
+		if(value.empty())
+			return std::vector<u8>{ AMF_STRING, 0x01 };
+
+		// strings are encoded as AMF_STRING, followed by their length and
+		// the string encoded as UTF8. thus, we start by serializing the
+		// length and overwriting the type marker of the serialized data
+		// with an AMF_STRING marker.
+		// length << 1 | 1 to mark string as non-reference
+		AmfInteger length(value.length() << 1 | 1);
+		auto buf = length.serialize();
+		buf[0] = AMF_STRING;
+
+		// now, append the actual string.
+		auto data = value.data();
+		buf.insert(buf.end(), data, data + value.length());
+
+		return buf;
 	}
 
 private:
