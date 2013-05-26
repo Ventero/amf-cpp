83c21df2d136f7cd919aa66b18e2bb99afe33c41
Ventero <ventero@ventero.de>
2013-05-26 22:03:36 +0200

    Add some more AmfObject tests

--- tests/object.cpp
+++ tests/object.cpp
@@ -1,7 +1,10 @@
 #include "gtest/gtest.h"
 
 #include "amf.hpp"
+#include "types/amfdouble.hpp"
+#include "types/amfinteger.hpp"
 #include "types/amfobject.hpp"
+#include "types/amfstring.hpp"
 
 void isEqual(const std::vector<u8>& expected, const AmfObject& value) {
 	v8 serialized = value.serialize();
@@ -121,5 +124,102 @@ TEST(ObjectSerializationTest, DynamicSealedAnonymousObject) {
 		0x06, 0x19, 0x64, 0x79, 0x6e, 0x61, 0x6d, 0x69, 0x63, 0x56, 0x61, 0x6c, 0x75, 0x65,
 		// end of dynamic members
 		0x01
+	}, obj);
+}
+
+TEST(ObjectSerializationTest, SealedNamedObject) {
+	AmfObjectTraits traits("de.ventero.AmfTest", false, false);
+	traits.attributes.push_back("sealedProp");
+	AmfObject obj(traits);
+
+	AmfString sealedValue("value");
+	obj.sealedProperties["sealedProp"] = &sealedValue;
+
+	isEqual(v8 {
+		0x0a, // AMF_OBJECT
+		0x13, // 0b10011, U29O-traits, not dynamic, 1 sealed property
+		// class-name UTF-8-vr "de.ventero.AmfTest"
+		0x25, 0x64, 0x65, 0x2e, 0x76, 0x65, 0x6e, 0x74, 0x65, 0x72, 0x6f, 0x2e,
+		0x41, 0x6d, 0x66, 0x54, 0x65, 0x73, 0x74,
+		// sealed property names
+		// UTF-8-vr "sealedProp"
+		0x15, 0x73, 0x65, 0x61, 0x6c, 0x65, 0x64, 0x50, 0x72, 0x6f, 0x70,
+		// sealed property values
+		// AmfString "value"
+		0x06, 0x0b, 0x76, 0x61, 0x6c, 0x75, 0x65
+		// no dynamic members
+	}, obj);
+}
+
+TEST(ObjectSerializationTest, DynamicSealedNamedObject) {
+	AmfObjectTraits traits("de.ventero.AmfTest", true, false);
+	traits.attributes.push_back("sealedProp");
+	AmfObject obj(traits);
+
+	AmfDouble sealedValue(3.14159);
+	obj.sealedProperties["sealedProp"] = &sealedValue;
+
+	AmfInteger dynamicValue(17);
+	obj.dynamicProperties["dynamicProp"] = &dynamicValue;
+
+	isEqual(v8 {
+		0x0a, // AMF_OBJECT
+		0x1b, // 0b11011, U29O-traits, dynamic, 1 sealed property
+		// class-name UTF-8-vr "de.ventero.AmfTest"
+		0x25, 0x64, 0x65, 0x2e, 0x76, 0x65, 0x6e, 0x74, 0x65, 0x72, 0x6f, 0x2e,
+		0x41, 0x6d, 0x66, 0x54, 0x65, 0x73, 0x74,
+		// sealed property names
+		// UTF-8-vr "sealedProp"
+		0x15, 0x73, 0x65, 0x61, 0x6c, 0x65, 0x64, 0x50, 0x72, 0x6f, 0x70,
+		// sealed property values
+		// AmfDouble 3.14159
+		0x05, 0x40, 0x09, 0x21, 0xf9, 0xf0, 0x1b, 0x86, 0x6e,
+		// dynamic members
+		// UTF-8-vr dynamicProp
+		0x17, 0x64, 0x79, 0x6e, 0x61, 0x6d, 0x69, 0x63, 0x50, 0x72, 0x6f, 0x70,
+		// AmfInteger 17
+		0x04, 0x11,
+		// end of dynamic members
+		0x01
+	}, obj);
+}
+
+TEST(ObjectSerializationTest, EmptySealedAnonymousObject) {
+	AmfObjectTraits traits("", false, false);
+	AmfObject obj(traits);
+
+	AmfInteger sealedValue(10);
+	obj.sealedProperties["sealedProp"] = &sealedValue;
+
+	isEqual(v8 {
+		0x0a, // AMF_OBJECT
+		0x03, // 0b0011, U29O-traits, not dynamic, 0 sealed properties
+		0x01 // class-name "" (anonymous object)
+	}, obj);
+}
+
+TEST(ObjectSerializationTest, SerializeOnlyPropsInTraits) {
+	AmfObjectTraits traits("", false, false);
+	traits.attributes.push_back("sealedProp");
+	AmfObject obj(traits);
+
+	AmfInteger sealedValue(0x05ffeffe);
+	obj.sealedProperties["sealedProp"] = &sealedValue;
+
+	// this should not be serialized as it's not part of the trait attributes
+	AmfString unusedValue("unused");
+	obj.sealedProperties["unusedProp"] = &unusedValue;
+
+	isEqual(v8 {
+		0x0a, // AMF_OBJECT
+		0x13, // 0b10011, U29O-traits, not dynamic, 1 sealed property
+		0x01, // class-name "" (anonymous object)
+		// sealed property names
+		// UTF-8-vr "sealedProp"
+		0x15, 0x73, 0x65, 0x61, 0x6c, 0x65, 0x64, 0x50, 0x72, 0x6f, 0x70,
+		// sealed property values
+		// AmfInteger 0x05ffeffe
+		0x04, 0x97, 0xff, 0xef, 0xfe
+		// no dynamic members
 	}, obj);
 }
