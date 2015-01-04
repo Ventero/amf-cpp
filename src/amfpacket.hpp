d36f87b09c057d97ee70a5df2136deb5e8c4ded6
Ventero <ventero@ventero.de>
2015-01-04 19:37:37 +0100

    Don't immediately serialize packet header/message values.

--- src/amfpacket.hpp
+++ src/amfpacket.hpp
@@ -3,6 +3,7 @@
 #define AMFPACKET_HPP
 
 #include "types/amfitem.hpp"
+#include "types/amfnull.hpp"
 
 namespace amf {
 
@@ -12,8 +13,9 @@ enum Amf0Marker : u8 {
 
 class PacketHeader : public AmfItem {
 public:
-	PacketHeader(std::string name, bool mustUnderstand, const AmfItem& value) :
-		name(name), mustUnderstand(mustUnderstand), value(value.serialize()) { };
+	template<typename T>
+	PacketHeader(std::string name, bool mustUnderstand, const T& value) :
+		name(name), mustUnderstand(mustUnderstand), value(new T(value)) { }
 
 	bool operator==(const AmfItem& other) const {
 		const PacketHeader* p = dynamic_cast<const PacketHeader*>(&other);
@@ -23,7 +25,8 @@ public:
 
 	v8 serialize() const {
 		v8 buf;
-		buf.reserve(2 + name.size() + 1 + 5 + value.size());
+		v8 value_data = value->serialize();
+		buf.reserve(2 + name.size() + 1 + 5 + value_data.size());
 		// Strings in AMF packets are always serialized as AMF0 UTF-8, i.e.
 		// U16 length (in network order) U8* value
 		// even though AMF3 encodes strings in a different format
@@ -36,24 +39,31 @@ public:
 		// we have to mark the value as AMF3 value, which is achieved by adding
 		// an AVMPLUS_OBJECT marker in front of the value. note that this counts
 		// towards the value's length.
-		v8 value_length = network_bytes<uint32_t>(value.size() + 1);
+		v8 value_length = network_bytes<uint32_t>(value_data.size() + 1);
 		buf.insert(buf.end(), value_length.begin(), value_length.end());
 		buf.push_back(AVMPLUS_OBJECT);
-		buf.insert(buf.end(), value.begin(), value.end());
+		buf.insert(buf.end(), value_data.begin(), value_data.end());
 
 		return buf;
 	}
 
-private:
+	template<typename T>
+	T& getValue() {
+		return value.as<T>();
+	}
+
 	std::string name;
 	bool mustUnderstand;
-	v8 value;
+
+private:
+	AmfItemPtr value;
 };
 
 class PacketMessage : public AmfItem {
 public:
-	PacketMessage(std::string targetUri, std::string responseUri, const AmfItem& value) :
-		target(targetUri), response(responseUri), value(value.serialize()) { };
+	template<typename T>
+	PacketMessage(std::string targetUri, std::string responseUri, const T& value) :
+		target(targetUri), response(responseUri), value(new T(value)) { }
 
 	bool operator==(const AmfItem& other) const {
 		const PacketMessage* p = dynamic_cast<const PacketMessage*>(&other);
@@ -63,7 +73,8 @@ public:
 
 	v8 serialize() const {
 		v8 buf;
-		buf.reserve(2 + target.size() + 2 + response.size() + 5 + value.size());
+		v8 value_data = value->serialize();
+		buf.reserve(2 + target.size() + 2 + response.size() + 5 + value_data.size());
 
 		v8 target_length = network_bytes<uint16_t>(target.size());
 		buf.insert(buf.end(), target_length.begin(), target_length.end());
@@ -73,18 +84,24 @@ public:
 		buf.insert(buf.end(), response_length.begin(), response_length.end());
 		buf.insert(buf.end(), response.begin(), response.end());
 
-		v8 value_length = network_bytes<uint32_t>(value.size() + 1);
+		v8 value_length = network_bytes<uint32_t>(value_data.size() + 1);
 		buf.insert(buf.end(), value_length.begin(), value_length.end());
 		buf.push_back(AVMPLUS_OBJECT);
-		buf.insert(buf.end(), value.begin(), value.end());
+		buf.insert(buf.end(), value_data.begin(), value_data.end());
 
 		return buf;
 	}
 
-private:
+	template<typename T>
+	T& getValue() {
+		return value.as<T>();
+	}
+
 	std::string target;
 	std::string response;
-	v8 value;
+
+private:
+	AmfItemPtr value;
 };
 
 class AmfPacket : public AmfItem {
